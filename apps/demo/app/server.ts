2210ca0dbece7a12cf686ab303d3b88037d41ba7
Mia <mia@mia.cx>
2025-10-27 19:11:52 +0100

    trim tailing slash

--- apps/demo/app/server.ts
+++ apps/demo/app/server.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 import { showRoutes } from "hono/dev";
+import { trimTrailingSlash } from "hono/trailing-slash";
 import { createApp } from "honox/server";
 
 const euterpe = createApp();
 
 const app = new Hono();
+
+app.use(trimTrailingSlash());
 
 app.route("/euterpe", euterpe);
 
