4ebcb9759f6b32ec27b6dc3fa00fe9655ee970fb
Mia <mia@mia.cx>
2025-10-27 20:50:06 +0100

    honox also doesn't work cuz doesn't support react which is necessary for shadcn

---
+++ apps/demo-hono/app/global.d.ts
@@ -0,0 +1,16 @@
+import type {} from "hono";
+
+import "@hono/react-renderer";
+
+declare module "hono" {
+    interface Env {
+        Variables: {};
+        Bindings: {};
+    }
+}
+
+declare module "@hono/react-renderer" {
+    interface Props {
+        title?: string;
+    }
+}
