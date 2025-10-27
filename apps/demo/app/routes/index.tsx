16ab41367625407e5cc943cac29ac327e7dad3cf
Mia <mia@mia.cx>
2025-10-27 19:50:00 +0100

    no more counter island

--- apps/demo/app/routes/index.tsx
+++ apps/demo/app/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createRoute } from "honox/factory";
-import Counter from "../islands/counter";
 
 export default createRoute((c) => {
   const name = c.req.query("name") ?? "Hono";
@@ -7,7 +6,6 @@ export default createRoute((c) => {
     <div class="py-8 text-center">
       <title>{name}</title>
       <h1 class="text-3xl font-bold">Hello, {name}!</h1>
-      <Counter />
     </div>,
   );
 });
